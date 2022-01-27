744eb457b12ddcc26d57e692d2a27479c52e4a8d
manas-simbaquartz <manas.mishra@simbaquartz.com>
2022-01-27 20:17:52 +0530

    Added Buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "./Components/Buttons";
+import "./root.css";
 
 const App = () => {
   return (
     <>
       <div>
-        <Button type="warning" title="mishra" />
+        <Button type="raised" title="Manas" className="manas" />
       </div>
     </>
   );
--- src/Components/Buttons/index.js
+++ src/Components/Buttons/index.js
@@ -1,13 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
+import "../../root.css";
 
 const Button = ({ title, type }) => {
   const types = {
     primary: {
       backgroundColor: "#0000ff",
+      color: "#fff",
+    },
+    success: {
+      backgroundColor: "#28A745",
+      color: "#fff",
     },
     warning: {
-      backgroundColor: "rgb(166 201 9)",
+      backgroundColor: "#FFC107",
+      color: "#000",
+    },
+    danger: {
+      backgroundColor: "#DC3545",
+      color: "#fff",
+    },
+    info: {
+      backgroundColor: "#17A2B8",
+      color: "#ffffff",
+    },
+    text: {
+      backgroundColor: "transparent",
+      color: "#0000ff",
+    },
+    ghost: {
+      backgroundColor: "transparent",
+      color: "#000",
+      border: "1px solid #000",
+    },
+    raised: {
+      backgroundColor: "#fff",
+      border: "1px solid #000",
+      boxShadow: "5px 5px 5px rgba(0, 0, 255, .2)",
     },
   };
   type ? console.log(type) : console.log("no type");
@@ -17,6 +46,9 @@ const Button = ({ title, type }) => {
         className="yuva-button"
         style={{
           backgroundColor: type ? types[type].backgroundColor : "inherit",
+          color: type ? types[type].color : "inherit",
+          border: type ? types[type].border : "inherit",
+          boxShadow: type ? types[type].boxShadow : "inherit",
         }}
       >
         {title}
